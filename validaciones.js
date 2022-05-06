d8255e2a7d9e2a1c98ff2103173d0691316ce527
franabarca <f.abarcacock@gmail.com>
2022-05-06 17:16:33 -0400

    Update 20

---
+++ validaciones.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+
+    $("#btnenviar").click(function(){
+        var nombre = $("#nombre").val();
+        var mail = $("#mail").val();
+        var mensaje = $("#mensaje").val();
+
+        if (nombre == ""){
+            $("#mensaje1").fadeIn();
+            return false;
+        }else{
+            $("#mensaje1").fadeOut();
+        }
+
+        if (mail == ""){
+            $("#mensaje2").fadeIn();
+            return false;
+        }else{
+            $("#mensaje2").fadeOut();
+        }
+
+        if (mensaje == ""){
+            $("#mensaje3").fadeIn();
+            return false;
+        }else{
+            $("#mensaje3").fadeOut();
+        }
+
+    });
+
+  });
