8948658ed8912dfd51d4e6e094a7e93c020c8955
franabarca <f.abarcacock@gmail.com>
2022-06-03 16:21:43 -0400

    tercero

--- PetShopDjango/PetShop/urls.py
+++ PetShopDjango/PetShop/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, ofertas, donar, index
 
 urlpatterns = [
-    path('', home,name='index'),
+    path('', home,name='home'),
+    path('pet/index.html', index,name='index'),
+    path('pet/index2.html', donar,name='donar'),
+    path('pet/ofertas.html', ofertas,name='ofertas'),
 ]
--- PetShopDjango/PetShop/views.py
+++ PetShopDjango/PetShop/views.py
@@ -4,4 +4,17 @@ from django.shortcuts import render
 
 def home(request):
 
-    return render(request, 'C:/ProyectosDjango/PetShopDjango/PetShop/templates/PetShop/index.html')
+    return render(request, 'pet/index.html')
+
+def index(request):
+
+    return render(request, 'pet/index.html')
+
+def donar(request):
+
+    return render(request, 'pet/index2.html')
+
+def ofertas(request):
+
+    return render(request, 'pet/ofertas.html')
+
